d3bc98b1359239816744d74bdfb6eae2ff61bcbd
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-03-03 22:17:41 -0300

    Feat: criação do login

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,7 +1,43 @@
+import {
+  Container,
+  LeftContainer,
+  RightContainer,
+  Title,
+  Form,
+  InputContainer,
+  Link,
+  Button,
+} from './styles';
+
+import Logo from '../../assets/Logo.svg';
+
 export function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span>
+          Acesse com seu <span>Login e senha.</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" />
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" />
+          </InputContainer>
+          <Link>Esqueci minha senha</Link>
+
+          <Button>Entra</Button>
+        </Form>
+        <Link>Não possui conta? Clique aqui.</Link>
+      </RightContainer>
+    </Container>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Login } from './containers/Login'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import GlobalStyles from "./styles/globalStyles";
+import { Login } from "./containers/Login";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Login />
-  </StrictMode>,
-)
+    <GlobalStyles />
+  </StrictMode>
+);
