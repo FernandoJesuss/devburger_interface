113fef5f7e146c3bba2e33fabad21391e004c85e
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-03-07 17:27:39 -0300

    Feat:Form validation

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -22,8 +22,8 @@ export function Login() {
 
   const schema = yup
 .object({
-  email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
+  email: yup.string().email("Digite um e-mail válido").required("O e-mail é obrigatório"),
+  password: yup.string().min(6, " A senha deve ter pelo menos 6 caracteres").required("Digite uma senha"),
 })
 .required();
 
@@ -35,6 +35,8 @@ const {
 } = useForm({
   resolver: yupResolver(schema),
 })
+
+console.log(errors);
 const onSubmit = (data) => console.log(data)
 
 
@@ -65,11 +67,13 @@ const onSubmit = (data) => console.log(data)
           <InputContainer>
             <label>Email</label>
             <input type="email"{...register("email")} />
+            <p>{errors?.email?.message}</p>
           </InputContainer>
 
           <InputContainer>
             <label>Senha</label>
             <input type="password" {...register("password")} />
+            <p>{errors?.password?.message}</p>
           </InputContainer>
           
 
