176870686c1a656e23b1498c9fc98f8fa57b4995
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-03-07 23:28:32 -0300

    Feat: User creation

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
+import { router } from "./routes";
 
 import GlobalStyles from "./styles/globalStyles";
-import { Login } from "./containers/Login";
+import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
    
+   
+    < RouterProvider router={router} />
     <GlobalStyles />
-    <Login />
     <ToastContainer autoClose={2000} theme="colored" />
     
   </StrictMode>
---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Login } from "../containers/Login"; 
+
+export const router = createBrowserRouter([
+{
+    path: "/",
+    element: <Login />,
+    
+},
+
+
+]);
