1834c694e56e81525fab1debe9fa0ab2617b650d
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-05-24 19:56:43 -0300

    Finishing Side Menu

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,7 +1,35 @@
+import { SignOut } from "@phosphor-icons/react";
+
+import Logo from '../../assets/Logo.svg';
+import { useUser } from "../../hooks/UserContext";
+import { navLinks } from "./navLinks";
+import { Container, NavLink, Footer, NavLinkContainer } from "./styles";
+
 export function SideNavAdmin() {
+    const { logout } = useUser();
     return (
-        <div>
-            <h3>Menu</h3>
-        </div>
+        <Container>
+            <img src={Logo} alt="Hamburger Logo DevBurger" />
+            <NavLinkContainer>
+             {navLinks.map( (link) =>(
+
+<NavLink key={link.id} to={link.path}>
+    {link.icon}
+    <span>
+    {link.label}
+    </span>
+
+</NavLink>
+             ))}
+
+            </NavLinkContainer>
+
+            <Footer>
+                <NavLink to="/login" onClick={logout}>
+                    <SignOut />
+                    <span>Sair</span>
+                </NavLink>
+            </Footer>
+        </Container>
     )
 }
