425e076a21cbb461c668c0bde6b0897108b2e8b9
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-05-26 13:54:27 -0300

    Feat:Adding functionality to the table

--- src/containers/Admin/Orders/row.jsx
+++ src/containers/Admin/Orders/row.jsx
@@ -12,12 +12,20 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { useState } from 'react';
 
+import { formatDate } from '../../../utils/formatDate';
+import { ProductImage, SelectStatus } from "./styles";
+import { orderStatusOptions } from './orderStatus';
+import { api } from "../../../services/api";
 
 
 
 export function Row(props) {
     const { row } = props;
     const [open, setOpen] = useState(false);
+
+    async function newStatusOrder(id, status) {
+await api.put(`orders/${id}`, { status });
+    }
 
     return (
         <>
@@ -34,9 +42,18 @@ export function Row(props) {
                 <TableCell component="th" scope="row">
                     {row.orderId}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.date}</TableCell>
-                <TableCell align="right">{row.status}</TableCell>
+                <TableCell>{row.calories}</TableCell>
+                <TableCell>{formatDate(row.date)}</TableCell>
+                <TableCell>
+                    <SelectStatus
+                     options={orderStatusOptions.filter( (status) => status.id !==0)}
+                      placeholder="Status"
+                      defaultValue={ orderStatusOptions.find(
+                         (status) => status.value === row.status || null,
+                        )}
+                        onChange={(status) => newStatusOrder( row.orderId, status.value) }
+                      />
+                </TableCell>
             </TableRow>
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -51,7 +68,7 @@ export function Row(props) {
                                         <TableCell>Quantidade</TableCell>
                                         <TableCell>Produto</TableCell>
                                         <TableCell>Categoria</TableCell>
-                                        <TableCell></TableCell>
+                                        <TableCell>Imagem do Produto</TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
@@ -63,7 +80,7 @@ export function Row(props) {
                                             <TableCell>{product.name}</TableCell>
                                             <TableCell>{product.category}</TableCell>
                                             <TableCell>
-                                              <img src={product.url} alt={product.name} />
+                                                <ProductImage src={product.url} alt={product.name} />
                                             </TableCell>
                                         </TableRow>
                                     ))}
