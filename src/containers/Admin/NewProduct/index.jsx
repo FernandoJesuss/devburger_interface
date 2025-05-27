9308f37731e89420b6c60ea65e1faf181ad6a9ed
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-05-27 18:45:32 -0300

    Feat:Creating the add products screen

--- src/containers/Admin/NewProduct/index.jsx
+++ src/containers/Admin/NewProduct/index.jsx
@@ -1,7 +1,55 @@
+import { useForm } from "react-hook-form"
+import { Image } from "@phosphor-icons/react"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+import { Container, SubmitButton, Form, InputGroup, Label, Input, LabelUpload, Select } from "./styles"
+
+const schema = yup
+    .object({
+        firstName: yup.string().required(),
+        age: yup.number().positive().integer().required(),
+    })
+    .required()
+
+
+
 export function NewProduct() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    })
+    const onSubmit = (data) => console.log(data)
     return (
-        <div>
-            <h2>Novos Produtos</h2>
-        </div>
+        <Container>
+            <Form>
+                <InputGroup>
+                    <Label>Nome</Label>
+                    <Input />
+                </InputGroup>
+
+                <InputGroup>
+                    <Label>Preço</Label>
+                    <Input />
+                </InputGroup>
+
+                <InputGroup>
+                    <LabelUpload>
+                        <Image />
+                        <input type="file" />
+                    </LabelUpload>
+                </InputGroup>
+
+                <InputGroup>
+                <Label>Categoria</Label>
+                <Select />
+                </InputGroup>
+
+                <SubmitButton>Adicionar Produto</SubmitButton>
+            </Form>
+        </Container>
     )
 }
