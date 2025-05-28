6013aafb8ed17f2cc8cfbedfb6f37f98ef1b37c0
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-05-27 23:52:20 -0300

    Finishing product screen validations

--- src/containers/Admin/NewProduct/index.jsx
+++ src/containers/Admin/NewProduct/index.jsx
@@ -6,20 +6,29 @@ import * as yup from "yup"
 import { api } from "../../../services/api"
 import { Container, ErrorMessage, SubmitButton, Form, InputGroup, Label, Input, LabelUpload, Select } from "./styles"
 import { useEffect, useState } from "react"
+import { toast } from "react-toastify"
 
-// const schema = yup
-//     .object({
-//         name: yup.string().required(),
-//         price: yup.number().positive().required(),
-//         category: yup.object().required(),
-//         file: yup.mixed(),
-//     });
+
 
 const schema = yup.object({
     name: yup.string().required("Nome é obrigatório"),
-    price: yup.number().positive("O preço deve ser um número positivo").required("Preço é obrigatório"),
+    price: yup.number().positive("O preço deve ser um número positivo").required("Preço é obrigatório").typeError("Preço é obrigatório"),
     category: yup.object().required("Categoria é obrigatória"),
-    file: yup.mixed().required("Um arquivo deve ser selecionado"), 
+    file: yup
+    .mixed()
+    .test("required", "Um arquivo deve ser selecionado", (value) => {
+        return value && value.length > 0;
+    })
+
+    .test("fileSize", "Carregue arquivo até 3mb", (value) => {
+        return value && value.length > 0 && value[0].size  <= 3 * 1024 * 1024; 
+    })
+
+    
+    .test("type", "Carregue apenas imagens PNG ou JPEG", (value) => {
+        return value && value.length > 0 && (value[0].type === "image/jpeg" || value[0].type === "image/png")
+        
+    }),
 }).required();
 
 
@@ -48,15 +57,30 @@ export function NewProduct() {
     } = useForm({
         resolver: yupResolver(schema),
     })
-    const onSubmit = (data) => {
-        console.log(data)
+    const onSubmit = async (data) => {
+        const productFormData = new FormData();
 
-    }
+        productFormData.append("name", data.name);
+        productFormData.append("price", data.price * 100);
+        productFormData.append("category_id", data.category.id);
+        productFormData.append("file", data.file[0]);
+
+        try {
+            await toast.promise(api.post("/products", productFormData), {
+                pending: "Adicionando o produto...",
+                success: "Produto criado com sucesso",
+                error: "Falha ao adicionar o produto, tente novamente",
+            });
+        } catch (error) {
+            console.error("Erro ao adicionar produto:", error);
+            toast.error("Ocorreu um erro ao adicionar o produto. Verifique os dados e tente novamente.");
+        }
+    };
     return (
         <Container>
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <InputGroup>
-                    <Label>Nome</Label>
+                    <Label>Nome do Produto</Label>
                     <Input type="text"  {...register("name")} />
                     <ErrorMessage>{errors?.name?.message}</ErrorMessage>
                 </InputGroup>
@@ -82,6 +106,7 @@ export function NewProduct() {
                         {fileName || "Upload do Produto"}
 
                     </LabelUpload>
+                     <ErrorMessage>{errors?.file?.message}</ErrorMessage>
                 </InputGroup>
 
                 <InputGroup>
@@ -89,7 +114,7 @@ export function NewProduct() {
                     <Controller
                         name="category"
                         control={control}
-                        render={(field) => (
+                        render={({field}) => (
                             <Select
                             {...field}
                                 options={categories}
@@ -100,6 +125,7 @@ export function NewProduct() {
                             />
                         )}
                     />
+                     <ErrorMessage>{errors?.category?.message}</ErrorMessage>
                 </InputGroup>
 
                 <SubmitButton>Adicionar Produto</SubmitButton>
