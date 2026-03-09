0709d4ff8333710c515954b6b065931a5f637407
fernandoJesuss <fernando_ffernando@hotmail.com>
2026-03-09 20:00:31 -0300

    fix: correct svg import case

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,22 +1,22 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export const ContainerButton = styled.button `
-width: 100%;
-height: 52px;
-border: 0;
-border-radius: 5px ;
-background-color: ${(props) => props.theme.purple};
-font-family: "Road Rage", sans-serif;
-font-size: 30px;
-color: ${(props) => props.theme.white};
+// export const ContainerButton = styled.button `
+// width: 100%;
+// height: 52px;
+// border: 0;
+// border-radius: 5px ;
+// background-color: ${(props) => props.theme.purple};
+// font-family: "Road Rage", sans-serif;
+// font-size: 30px;
+// color: ${(props) => props.theme.white};
 
-&:hover {
-background-color: ${(props) => props.theme.secondDarkPurple};
-border: 1px solid ${(props) => props.theme.white};
-}
+// &:hover {
+// background-color: ${(props) => props.theme.secondDarkPurple};
+// border: 1px solid ${(props) => props.theme.white};
+// }
 
-&:active {
-    background-color: #6f355d;
-}
+// &:active {
+//     background-color: #6f355d;
+// }
 
-`;
+// `;
